388f166f7566ba22e7722712533e4834d81160c2
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 12:01:39 -0400

    Removed original test assertion

--- test/cdk.test.ts
+++ test/cdk.test.ts
@@ -11,7 +11,7 @@ test('SQS Queue Created', () => {
     // THEN
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
+  // template.hasResourceProperties('AWS::SQS::Queue', {
+  //   VisibilityTimeout: 300
+  // });
 });
