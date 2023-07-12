5fc9c5c1e9a9715c2c5643e0e0652d6d1134463f
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 11:27:58 -0400

    Cleaned up bin

--- bin/index.ts
+++ bin/index.ts
@@ -1,19 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib';
+// AWS CDK Library
+import { App } from 'aws-cdk-lib'
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// Local imports
+import { CdkStack } from '../lib'
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Initialize the CDK App
+const app = new App()
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// Add the Stack to the App
+new CdkStack(app, 'CdkStack')
