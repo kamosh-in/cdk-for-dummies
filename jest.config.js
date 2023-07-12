93500eb990183f0bc6d9f17cb13facb7fdde97b9
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 09:10:13 -0400

    Using ts-jest init preset

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
 };
