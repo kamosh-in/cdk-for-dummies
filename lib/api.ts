9a84df38cecf9b8723774af929ae4ab0dc87bb88
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 13:32:05 -0400

    Added Hello World Function to Api Construct

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,10 @@
 // AWS CDK Libraries
 import { Stack } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+
+// Additional Libraries
+import path from 'path'
 
 // Props for the Api Construct
 export interface ApiProps {}
@@ -10,5 +14,8 @@ export class Api extends Construct {
   constructor(scope: Stack, id: string, props?: ApiProps) {
     super(scope, id)
 
+		new NodejsFunction(this, 'HelloFunction', {
+			entry: path.resolve(__dirname,'../src/hello.ts')
+		})
   }
 }
