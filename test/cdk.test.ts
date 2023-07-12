96fcf0643d3954c259e6568fca9d309aeefc7112
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 09:53:47 -0400

    Develop base configuration (#3)
    * Use tsconfig.json from Lambda guide
    
    * Added in development message
    
    * Updated package versions
    
    * Using ts-jest init preset
    
    * Simplified cdk config file
    
    * Removed .npmignore
    
    * Removed unnecessary Git ignores
    
    * Further simplified .gitignore
    
    * Renamed base bin and lib files to "index"
    
    * Modified cdk.json to comply with renaming bin

--- test/cdk.test.ts
+++ test/cdk.test.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import * as Cdk from '../lib/cdk-stack';
+import * as Cdk from '../lib';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-stack.ts
