586b1b90c77c4acf65c7bea8179593b04fc6729f
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-12 11:52:22 -0400

    Stack references Database

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,14 @@
+// AWS CDK Library
 import { App, Stack, StackProps } from 'aws-cdk-lib'
+
+// Local imports
+import { Database } from './database'
 
 export class CdkStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
 		
+		// Initialize Constructs that build the Application
+		new Database(this, 'Database')
   }
 }
