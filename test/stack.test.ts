b7d88cc8b89968d9ebfb7bb3825ae34f6a76095b
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-14 08:25:22 -0500

    Added template assertion tests

---
+++ test/stack.test.ts
@@ -0,0 +1,59 @@
+// AWS CDK Packages
+import { App } from 'aws-cdk-lib'
+
+// CDK Assertion Testing
+import { Template } from 'aws-cdk-lib/assertions'
+
+// Local Packages
+import { DummyStack } from '../lib'
+
+const app = new App()
+const stack = new DummyStack(app, 'DummyStack')
+const template = Template.fromStack(stack)
+
+test('DynamoDB Table Created', () => {
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+		AttributeDefinitions: [
+		 {
+			AttributeName: 'Id',
+			AttributeType: 'S'
+		 }
+		],
+		BillingMode: 'PAY_PER_REQUEST',
+		KeySchema: [
+			{
+			 AttributeName: 'Id',
+			 KeyType: 'HASH'
+			}
+		 ],
+  })
+})
+
+test('DynamoDB Table Production-ready', () => {
+  template.hasResource('AWS::DynamoDB::Table', {
+		DeletionPolicy: 'Retain',
+		UpdateReplacePolicy: 'Retain',
+  })
+})
+
+test('API Gateway Created', () => {
+  template.hasResource('AWS::ApiGateway::RestApi', {})
+
+	template.hasResourceProperties('AWS::ApiGateway::Resource', {
+		PathPart: '{proxy+}',
+	})
+
+	template.hasResourceProperties('AWS::ApiGateway::Method', {
+		Integration: {
+			IntegrationHttpMethod: 'POST',
+			Type: 'AWS_PROXY',
+		},
+		HttpMethod: 'ANY',
+	})
+})
+
+test('API Gateway Production-ready', () => {
+	template.hasResourceProperties('AWS::ApiGateway::Stage', {
+		StageName: 'prod',
+	})
+})
