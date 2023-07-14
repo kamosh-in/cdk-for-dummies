3a5e991ed2eed87962240680529684ac8ef737f2
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-14 08:48:04 -0500

    Added logging for context and callback

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -8,8 +8,10 @@ import { ScanCommand } from '@aws-sdk/client-dynamodb'
 import { ddbDocClient } from '../lib/aws'
 import { TABLE_NAME } from '../lib/env'
 
-export const handler: APIGatewayProxyHandler = async (event) => {
-	console.log(`EVENT: \n ${JSON.stringify(event, null, 2)}`)
+export const handler: APIGatewayProxyHandler = async (event, context, callback) => {
+	console.info(`EVENT: \n ${JSON.stringify(event, null, 2)}`)
+	console.info(`CONTEXT: \n ${JSON.stringify(context, null, 2)}`)
+	console.info(`CALLBACK: \n ${JSON.stringify(callback, null, 2)}`)
 
 	const result = await ddbDocClient.send(new ScanCommand({
 		AttributesToGet: [
@@ -21,8 +23,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: 'This is the Scan result',
-			result: result.Items
+			Items: result.Items
 		}, null, 2)
 	}
 };
