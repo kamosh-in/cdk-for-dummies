a4cef3b69c0000027f4c430bc8b077f062c20e56
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-18 09:27:52 -0400

    Added Update Functionality

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -2,30 +2,58 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 // AWS SDK Modules
-import { } from '@aws-sdk/lib-dynamodb'
+import { PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb'
 
 // Local Modules
-import { } from '../lib/aws'
-import { } from  '../lib/env'
+import { ddbDocClient } from '../lib/aws/'
+import { TABLE_NAME } from  '../lib/env'
 
-const getInput = () => {
-
-}
-
-const getCommand = () => {
-
-}
-
-const getResult = () => {
-
-}
-
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-	console.log(`EVENT:\n${JSON.stringify(event, null, 2)}`)
+// Get input for the Put Command
+const getInput = (event: APIGatewayProxyEvent, TableName: string): PutCommandInput => {
+	const { Item } = JSON.parse(event.body as string) && event.pathParameters
 	return {
-		statusCode: 200,
+
+		// Prevents creating a new item
+		ConditionExpression: 'attribute_exists(Id)',
+
+		Item,
+		TableName,
+	}
+}
+
+// Get PutCommand for the DynamoDB Document Client to send
+const getCommand = (input: PutCommandInput): PutCommand => {
+	return new PutCommand(input)
+}
+
+// Get result for API Gateway
+const getResult = (statusCode: number): APIGatewayProxyResult => {
+	let message
+	if (statusCode == 200)
+		message = 'Update succeeded'
+	else
+		message = 'Update failed'
+	return {
+		statusCode,
 		body: JSON.stringify({
-			message: 'Hello World!'
-		})
+			message
+		}, null, 2)
+	}
+}
+
+// Handler for PUT /{Id}
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	try {
+		console.log(`EVENT:\n${JSON.stringify(event, null, 2)}`)
+		const input = getInput(event, TABLE_NAME)
+		console.log(`INPUT:\n${JSON.stringify(input, null, 2)}`)
+		const command = getCommand(input)
+		console.log(`COMMAND:\n${JSON.stringify(command, null, 2)}`)
+		const response = await ddbDocClient.send(command)
+		console.log(`RESPONSE:\n${JSON.stringify(response, null, 2)}`)
+		return getResult(200)
+	} catch (error) {
+		console.log(`ERROR:\n${JSON.stringify(error, null, 2)}`)
+		return getResult(400)
 	}
 }
