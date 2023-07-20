d5633ef325b3fc894bce66723a9a927d6b2fd03c
Matthew Thompson <mrmatthewthompson@icloud.com>
2023-07-20 08:31:57 -0400

    Added Read functionality

--- src/handlers/read.ts
+++ src/handlers/read.ts
@@ -2,30 +2,51 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 // AWS SDK Modules
-import { } from '@aws-sdk/lib-dynamodb'
+import { ScanCommand, ScanCommandInput, ScanCommandOutput } from '@aws-sdk/lib-dynamodb'
 
 // Local Modules
-import { } from '../lib/aws'
-import { } from  '../lib/env'
+import { ddbDocClient } from '../lib/aws'
+import { TABLE_NAME } from  '../lib/env'
 
-const getInput = () => {
-
+const getInput = (event: APIGatewayProxyEvent, TableName: string): ScanCommandInput => {
+	return {
+		AttributesToGet: [
+			'Id',
+		],
+		TableName,
+	}
 }
 
-const getCommand = () => {
-
+const getCommand = (input: ScanCommandInput): ScanCommand => {
+	return new ScanCommand(input)
 }
 
-const getResult = () => {
-
+const getResult = (statusCode: number, response?: ScanCommandOutput): APIGatewayProxyResult => {
+	let message
+	if (statusCode === 200)
+		message = response
+	else
+		message = 'Read failed'
+	return {
+		statusCode,
+		body: JSON.stringify({
+			message
+		}, null, 2)
+	}
 }
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-	console.log(`EVENT:\n${JSON.stringify(event, null, 2)}`)
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Hello World!'
-		})
+	try {
+		console.log(`EVENT:\n${JSON.stringify(event, null, 2)}`)
+		const input = getInput(event, TABLE_NAME)
+		console.log(`INPUT:\n${JSON.stringify(input, null, 2)}`)
+		const command = getCommand(input)
+		console.log(`COMMAND:\n${JSON.stringify(command, null, 2)}`)
+		const response = await ddbDocClient.send(command)
+		console.log(`RESPONSE:\n${JSON.stringify(response, null, 2)}`)
+		return getResult(200, response)
+	} catch (error) {
+		console.log(`ERROR:\n${JSON.stringify(error, null, 2)}`)
+		return getResult(400)
 	}
 }
